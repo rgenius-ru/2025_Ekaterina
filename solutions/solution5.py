708aa712f51b383d466c8013b4dcc254a3ecc093
Kat-hash-coder <kristi-85@ya.ru>
2025-03-11 18:37:38 +0300

    new branch

---
+++ solutions/solution5.py
@@ -0,0 +1,47 @@
+# # print("привет")
+# # number=str(15.8)
+# # print(number)
+
+# # L=5
+# # R=3
+# # H=2.5
+# # print("V=", L*R*H)
+# # n='Python '
+# # reversed_string = n[::-1]
+# # print(n*50)
+
+# # number1 = int(input('Введите числo1: '))
+# # number2 = int(input('Введите числo2: '))
+# # print("Разница чисел:", number1-number2)
+
+# name = (input('Имя: '))
+# age = int(input('Возраст: '))
+# oldage = int(age+7)
+# print("Привет, "+ name+"! Через 7 лет вам будет "+ str(oldage))
+
+# name =(input("Имя: "))
+# age = int(input('Возраст: '))
+# hobby = (input('Любимое занятие: '))
+# # print(name,'говорит:', "Обожаю", hobby+"!\nЭто моё самое большое увлечение!")
+
+# message=f"""
+# ==={name}===
+# Возраст {age}
+# Хобби: {hobby}
+
+# {name}, говорит: Обожаю {hobby}!
+# 'Это моё самое большое увлечение!'"""
+
+# print(message)
+
+# name =(input("Имя персонажа "))
+# text =(input("Текст сообщения ")) 
+# L = len(text)
+# headline = (-*L)
+# message=f"""
+# _________________
+# _________________
+# {headline}
+# Говорит: {name}"""
+
+# print(message)
