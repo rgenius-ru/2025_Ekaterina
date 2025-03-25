918d0f68419dbfd9c91df4b42500308bae305c5e
Kat-hash-coder <kristi-85@ya.ru>
2025-03-25 14:06:23 +0300

    fin

---
+++ solutions/solution9.py
@@ -0,0 +1,12 @@
+# print(15!=20)
+# print(100>=99)
+# print("hello" == "Hello")  # False (регистр имеет значение)
+# print(3.0 == 3)            # True (сравнение int и float)
+# age1 = int(input('Возраст1: '))
+# age2 = int(input('Возраст2: '))
+# print("Одного ли возраста вы?:", age1==age2)
+
+limit = int(input('Ваш доход: '))
+if limit >= 1000000:
+    print('Вы миллионер')  # TAB
+print("Спасибо за обращение!")
