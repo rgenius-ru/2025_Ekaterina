549fa17c4889350dfa442039ad30d7e444aabe83
rg <rgenius@yandex.ru>
2025-04-03 19:09:35 +0300

    add lesson 11

---
+++ lessons/lesson11.py
@@ -0,0 +1,35 @@
+# Операторы and, or, not
+
+
+
+# price = 100_000
+# color = 'orange' 
+# if price <= 100_000 and (color == 'red' or color == 'orange'):
+#     print('Купим')
+# else:
+#     print('Не купим')
+
+
+text = 'hello'
+if not text == 'hello': # True -> False, False -> True
+    print('Не приветствую!')
+
+
+# not 
+# not 0 -> 1
+# not 1 -> 0
+
+# and
+# 0 and 0 -> 0
+# 0 and 1 -> 0
+# 1 and 0 -> 0
+# 1 and 1 -> 1
+
+# or
+# 0 or 0 -> 0
+# 0 or 1 -> 1
+# 1 or 0 -> 1
+# 1 or 1 -> 1
+
+
+# price <= 100_000 and (color == 'red' or color == 'orange')
